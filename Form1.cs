231b9b8c7e9070c76e5d7aef1897c28d08116e0f
KaanBN <kaan.mlnym@hotmail.com>
2021-12-14 17:25:17 +0300

    fixed login username input

--- Form1.cs
+++ Form1.cs
@@ -21,12 +21,30 @@ namespace Twitter_Bot
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
             try
             {
+                //first login button
+                IWebElement button = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("//a[@data-testid='loginButton']")));
+                button.Click();
+
                 // locate mail input location
-                IWebElement mail = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("//input[@autocomplete = 'username']")));
+                IWebElement mail = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("//input[@dir = 'auto']")));
                 mail.SendKeys(user_mail + Keys.Enter);
 
+                //locate input only one input apears usually
+                IWebElement telephone_input = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("//input[@dir = 'auto']")));
+
+                //get name of input
+                String input_name = telephone_input.GetAttribute("name");
+
+                //if input name is text it means that wants username of tel
+                if (input_name == "text")
+                {
+                    telephone_input.SendKeys("ServanBalar" + Keys.Enter);
+                }
+
+                //locate pass input and write pass
                 IWebElement pass = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Name("password")));
                 pass.SendKeys(user_pass + Keys.Enter);
+
             }
             catch (Exception)
             {
@@ -39,21 +57,19 @@ namespace Twitter_Bot
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Console.WriteLine();
             ChromeOptions options = new ChromeOptions();
             options.BinaryLocation = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
-            options.AddArgument("--user-data-dir=" + System.IO.Path.GetTempPath() + "\\twitter_bot_user_data\\" + textBox1.Text.ToString());
+            //options.AddArgument("--user-data-dir=" + System.IO.Path.GetTempPath() + "\\twitter_bot_user_data\\" + textBox1.Text.ToString());
             //options.AddArguments("headless");
 
             ChromeDriverService driverService = ChromeDriverService.CreateDefaultService("C:\\Program Files\\Google\\Chrome\\Application");
             driverService.HideCommandPromptWindow = true;
 
             WebDriver driver = new ChromeDriver(driverService, options);
-            string url = "https://twitter.com/i/flow/login";
+            string url = "https://twitter.com/";
 
             driver.Navigate().GoToUrl(url);
             login(driver, textBox1.Text.ToString(), textBox2.Text.ToString());
-            Thread.Sleep(1000);
             Console.WriteLine("Successfully logged in");
 
 
